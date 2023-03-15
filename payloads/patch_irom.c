1d6504cf42c1054f6eca01e119ee10ce98a4a174
Jonas Schwöbel <jonasschwoebel@yahoo.de>
2023-03-15 03:22:24 +0100

    Payload Improvements
    - PatchIROM: patch the T114 IROM and keep UART-Bitbang in comment
    - DumpSBKviaUSB: transfer data via USB demo

--- payloads/patch_irom.c
+++ payloads/patch_irom.c
@@ -14,13 +14,15 @@
 // end original header
 
 #include <stdint.h>
-#include "printf.h"
+//#include "printf.h"
+#include "tegra30_uart.h"
 #include "common.h"
 
-#define BOOTROM_START_POST_IPATCH	0xfff01004
+#define BOOTROM_START_POST_IPATCH	0xfff01008
 #define DESIRED_SECURITY_MODE		3
 #define IPATCH_SLOT					1
-#define IROM_PATCH_ADDRESS			0xfff01CD4
+#define IROM_PATCH_ADDRESS			0xfff022ac
+
 #define PMC_SCRATCH0				(0x50)
 
 /* ipatch hardware */
@@ -33,34 +35,126 @@
 #define APBDEV_PMC_SCRATCH42_0		(0x144)
 
 
+#define GPIO_BASE (0x6000d000)
+#define GPIO_PORT_A (0x000)
+#define GPIO_PORT_B (0x004)
+#define GPIO_PORT_BB (0x60C)
+#define GPIO_CNF (0x00)
+#define GPIO_OE (0x10)
+#define GPIO_OUT (0x20)
+
 // General next-stage image entry point type.
 typedef void (*entry_point)(void);
 
 void ipatch_word(uint8_t slot, uint32_t addr, uint16_t new_value);
 
+void send_zero();
+
+void send_one();
+
+void send_byte(uint8_t b);
+
+#define BOOTROM_START			0xfff00000
+#define BOOTROM_SIZE_T114		0x10000
+
+
+
+
 void main()
 {
 	entry_point start;
-	
-	printf("Hello there :)\r\n");
 
-	reg_write(PMC_BASE, APBDEV_PMC_SCRATCH42_0, 0);
-
-
-	/* Patch the getSecurityMode function to always return 3 (production non-secure). */
-	printf("overriding getSecurityMode function to always return 3 (production non-secure)...\r\n");
 	ipatch_word(IPATCH_SLOT, IROM_PATCH_ADDRESS, 0x2000 | DESIRED_SECURITY_MODE);
 
-	/* Clear bit0 to indicate that this is a fresh boot, and then set bit2 to trigger RCM. */
-	printf("writing PMC_SCRATCH0 to trigger RCM mode after soft reset...\r\n");
-	reg_write(PMC_BASE, PMC_SCRATCH0, (1 << 1)); // wrong 2));
-
-	printf("jumping to 0xfff01004...\r\n");
-	reg_write(PMC_BASE, APBDEV_PMC_SCRATCH42_0, 0);
-	/* Jump back into the bootloader immediately after ipatches are applied
-	   to simulate a normal coldboot as best we can. :) */
 	start = (entry_point)BOOTROM_START_POST_IPATCH;
 	start();
+
+	//uart_init();
+
+	
+	//reg_set(GPIO_BASE + GPIO_PORT_A, GPIO_CNF, 1 << 7);
+	/*
+	reg_set(GPIO_BASE + GPIO_PORT_A, GPIO_OE, 1 << 7);
+	reg_set(GPIO_BASE + GPIO_PORT_A, GPIO_OUT, 1 << 7);
+	
+	//reg_set(GPIO_BASE + GPIO_PORT_B, GPIO_CNF, 1 << 6);
+	reg_set(GPIO_BASE + GPIO_PORT_B, GPIO_OE, 1 << 6);
+	reg_set(GPIO_BASE + GPIO_PORT_B, GPIO_OUT, 1 << 6);
+	
+	uart_print("hello there\r\n");
+	send_byte(0x55);
+	send_byte(0x55);
+	send_byte(0x55);
+	send_byte(0x55);
+	
+	uint8_t *irom_base = (uint8_t*)BOOTROM_START;
+	for (uint32_t i = 0; i < BOOTROM_SIZE_T114; i++) {
+		send_byte(irom_base[i]);
+		for (int i = 0; i < 216000000 / 216 / 55 * 4; i++) {;}
+		send_byte(irom_base[i]);
+		for (int i = 0; i < 216000000 / 216 / 55 * 4; i++) {;}
+		send_byte(irom_base[i]);
+		
+		// give uart some time to synchronize in case timing is a bit of.
+		for (int i = 0; i < 216000000 / 216 / 55 * 4; i++) {;}
+	}
+	
+	uint8_t counter = 0;
+	while(1) {
+		send_byte(counter++);
+		//send_byte(0x55);
+		for (int i = 0; i < 6000000; i++) {;}
+	}
+	
+	
+	
+	//reg_set(PMC_BASE, PMC_SCRATCH0, 2);
+	reg_set(PMC_BASE, 0, 0x10);
+	
+
+	while(1) {
+		uart_print("Hello there :)\r\n");
+		for (int i = 0; i < 600000; i++) {
+			asm("nop");
+		}
+	}
+	
+	
+	while(1) {
+		;
+	}
+	*/
+}
+
+void send_byte(uint8_t b) {
+	// start bit
+	send_zero();
+	for (int i = 0; i < 216000000 / 216 / 55; i++) {;}
+	
+	// data
+	for (uint32_t i = 0; i < 8; i++) {
+		if (b & 1) {
+			send_one();
+		} else {
+			send_zero();
+		}
+		b >>= 1;
+		for (int i = 0; i < 216000000 / 216 / 55; i++) {;}
+	}
+	
+	// stop bit
+	send_one();
+	for (int i = 0; i < 216000000 / 216 / 55; i++) {;}
+}
+
+void send_zero() {
+	reg_write(PINMUX_BASE, PINMUX_AUX_SDMMC3_DAT1_0, 0b00000100); /* tx */
+	reg_write(PINMUX_BASE, PINMUX_AUX_SDMMC3_CMD_0, 0b00100100); /* rx */
+}
+
+void send_one() {
+	reg_write(PINMUX_BASE, PINMUX_AUX_SDMMC3_DAT1_0, 0b00000110); /* tx */
+	reg_write(PINMUX_BASE, PINMUX_AUX_SDMMC3_CMD_0, 0b00100110); /* rx */
 }
 
 /**
